86c3bfce2c14d6570f0fff3bfd0cd8c51ae213c2
longnguyen7731 <longnguyen7731@gmail.com>
2025-11-16 01:04:30 -0500

    added more listings

--- UI_Code/src/lib/stores/listings.js
+++ UI_Code/src/lib/stores/listings.js
@@ -18,6 +18,78 @@ export const listings = writable([
         year: 2015,
         category: "Cars",
         img: "/demo/civic.jpg"
+    },
+    {
+    id: 3,
+    title: "Samsung 32\" TV",
+    price: 320,
+    location: "Cincinnati",
+    year: 2019,
+    category: "Electronics",
+    img: "/demo/samsung32tv.jpg"
+    },
+    {
+    id: 4,
+    title: "MacBook Pro 16\"",
+    price: 1200,
+    location: "Cincinnati",
+    year: 2020,
+    category: "Electronics",
+    img: "/demo/macbook.jpg"
+    },
+    {
+    id: 5,
+    title: "Nissan Altima",
+    price: 6700,
+    location: "Cincinnati",
+    year: 2014,
+    category: "Cars",
+    img: "/demo/altima.jpg"
+    },
+    {
+    id: 6,
+    title: "Dining Table Set",
+    price: 250,
+    location: "Cincinnati",
+    year: 2018,
+    category: "Furniture",
+    img: "/demo/tableset.jpg"
+    },
+    {
+    id: 7,
+    title: "Gaming PC (RTX 3050)",
+    price: 900,
+    location: "Cincinnati",
+    year: 2022,
+    category: "Electronics",
+    img: "/demo/gamingpc.jpg"
+    },
+    {
+    id: 8,
+    title: "Mountain Bike",
+    price: 180,
+    location: "Cincinnati",
+    year: 2020,
+    category: "Sports",
+    img: "/demo/mountainbike.jpg"
+    },
+    {
+    id: 9,
+    title: "Sofa – 3 Seater",
+    price: 350,
+    location: "Cincinnati",
+    year: 2019,
+    category: "Furniture",
+    img: "/demo/sofa3seat.jpg"
+    },
+    {
+    id: 10,
+    title: "Nintendo Switch",
+    price: 220,
+    location: "Cincinnati",
+    year: 2021,
+    category: "Electronics",
+    img: "/demo/nintendoswitch.jpg"
     }
     // Add more sample data later…
 ]);
